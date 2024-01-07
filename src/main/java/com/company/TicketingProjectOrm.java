775a56f0e3fa23f4c387f02cfd97a6ac249c2a07
sa-023 <mavisky.023@gmail.com>
2024-01-06 19:33:23 -0600

    Initial commit

---
+++ src/main/java/com/company/TicketingProjectOrm.java
@@ -0,0 +1,12 @@
+package com.company;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectOrm {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingProjectOrm.class, args);
+	}
+
+}
